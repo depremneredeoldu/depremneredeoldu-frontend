ed778be9f97db3b9b4ec3d901814b10e429e393b
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-02 01:32:35 +0300

    Added a function which allows us to load JSON data from API url

---
+++ assets/js/dataFromAPI.js
@@ -0,0 +1,18 @@
+var myGeocode;
+
+let url = "https://api.depremneredeoldu.com/earthquakes";
+
+var xhr = new XMLHttpRequest();
+
+function loadJSON(url){
+    
+    // async==False beacause we want to store the data fetched into a js variable
+    xhr.open("GET", url, false);
+    xhr.send(null);
+
+     // set json data into a global variable
+    myGeocode = JSON.parse(xhr.responseText);
+
+}
+
+loadJSON(url);
