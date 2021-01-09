154ebcc5fc70d64145e184ac6d36f8ebec469ce2
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-09 18:57:05 +0300

    Add a config file to get the access token from module The access token used here is a private key. It will work only on the domain depremneredeoldu.com If you want to test it, you just need to sign up on mapbox.com and get your own access token and change it in this file.

---
+++ assets/js/configMap.js
@@ -0,0 +1,3 @@
+// private key for the domain depremneredeoldu.com, it will work only on depremneredeoldu.com
+
+const ACCESSTOKEN = "pk.eyJ1IjoiYWxpY2FueXVrc2VsIiwiYSI6ImNrajIxZzk1azBvdnUzMHA4a2l6ZDRuaHgifQ.h2r1WhCZ9tJ9AIBIt3lLwQ";
