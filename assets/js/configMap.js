7987bfae32118d8376b6f6f9ef5e27c582422a70
Alican Yüksel <alicanyuksel@outlook.com>
2021-02-03 00:00:14 +0300

    Refresh the access token

--- assets/js/configMap.js
+++ assets/js/configMap.js
@@ -1,4 +1,4 @@
 // private key for the domain depremneredeoldu.com, it will work only on depremneredeoldu.com
 
-const ACCESSTOKEN = "pk.eyJ1IjoiYWxpY2FueXVrc2VsIiwiYSI6ImNrajF2bWYxeTM0a2gyd3NjeTB4YjV5ZmMifQ.0o7JDdp80nx4cx6vOM_Awg";
+const ACCESSTOKEN = "pk.eyJ1IjoiYWxpY2FueXVrc2VsIiwiYSI6ImNrajIxZzk1azBvdnUzMHA4a2l6ZDRuaHgifQ.h2r1WhCZ9tJ9AIBIt3lLwQ";
 const defaultRangeValue = 50;
