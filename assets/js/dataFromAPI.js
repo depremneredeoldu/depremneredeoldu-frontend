658129a723937eeae85c836c3a464e92e38a2cd8
alicanyuksel <alicanyuksel@outlook.com>
2023-01-31 03:52:42 +0100

    Update api url

--- assets/js/dataFromAPI.js
+++ assets/js/dataFromAPI.js
@@ -1,18 +1,18 @@
 var myGeocode;
 
-let url = "https://api.depremneredeoldu.com/earthquakes";
+let url = "https://api.depremneredeoldu.com/v1/earthquakes";
 
 var xhr = new XMLHttpRequest();
 
 function loadJSON(url) {
-    // async==False beacause we want to store the data fetched into a js variable
-    xhr.open("GET", url, false);
-    xhr.send(null);
+  // async==False beacause we want to store the data fetched into a js variable
+  xhr.open("GET", url, false);
+  xhr.send(null);
 
-    // set json data into a global variable
-    myGeocode = JSON.parse(xhr.responseText);
-    // reverse for sorting the date
-    myGeocode["earthquakes"].reverse();
+  // set json data into a global variable
+  myGeocode = JSON.parse(xhr.responseText);
+  // reverse for sorting the date
+  myGeocode["earthquakes"].reverse();
 }
 
 loadJSON(url);
